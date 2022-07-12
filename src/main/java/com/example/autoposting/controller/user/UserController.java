7763b0845ced4a904a86caf1d088e7346679d885
William0751 <mybrokersearchwebsite@gmail.com>
2022-07-13 01:23:45 +0400

    styling

--- src/main/java/com/example/autoposting/controller/user/UserController.java
+++ src/main/java/com/example/autoposting/controller/user/UserController.java
@@ -22,9 +22,11 @@ public class UserController {
 
     @GetMapping
     public String allUsers(ModelMap modelMap) {
+
         List<User> all = userService.findAll();
         modelMap.addAttribute("users", all);
         return "user/all-users";
+
     }
 
     @GetMapping("/save")
@@ -34,26 +36,38 @@ public class UserController {
 
     @PostMapping("/save")
     public String saveAcc(@ModelAttribute SaveUserRequest userRequest, ModelMap modelMap) {
+
         if (userRequest.getName().trim().equals("") || userRequest.getName() == null) {
             modelMap.addAttribute("missedName", "Please input user's name");
             return "user/save-user";
         }
+
         if (userRequest.getSurname().trim().equals("") || userRequest.getSurname() == null) {
             modelMap.addAttribute("missedSurname", "Please input user's surname");
             return "user/save-user";
         }
+
         if (userRequest.getToken().trim().equals("") || userRequest.getToken() == null) {
             modelMap.addAttribute("missedToken", "Please input user's token");
             return "user/save-user";
         }
+
+        if (userRequest.getProfileId() == null) {
+            modelMap.addAttribute("missedId", "Please input user's profile id");
+            return "user/save-user";
+        }
+
         if (userRequest.getUserType() == null) {
             modelMap.addAttribute("missedProfileType", "Please select user's profile type");
             return "user/save-user";
         }
+
         User user = mapper.map(userRequest, User.class);
+
         if (userRequest.getId() != 0) {
             user.setId(userRequest.getId());
         }
+
         switch (userRequest.getUserType()) {
             case "INSTAGRAM":
                 user.setProfileType(UserType.INSTAGRAM);
@@ -67,28 +81,37 @@ public class UserController {
             default:
                 modelMap.addAttribute("missedProfileType", "Select Profile Type");
         }
+
         userService.save(user);
         return "redirect:/users";
+
     }
 
 
     @GetMapping("/edit/{id}")
     public String editUser(@PathVariable int id, ModelMap modelMap) {
+
         Optional<User> userById = userService.findById(id);
+
         if (userById.isEmpty()) {
             return "redirect:/users/";
         }
+
         User user = userById.get();
         modelMap.addAttribute("user", user);
         return "user/edit-user";
+
     }
 
     @GetMapping("/delete/{id}")
     public String deleteUser(@PathVariable int id) {
+
         Optional<User> userById = userService.findById(id);
+
         if (userById.isPresent()) {
             userService.deleteById(id);
         }
+
         return "redirect:/users";
     }
 }
